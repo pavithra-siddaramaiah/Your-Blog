ce93d553cc3d178559657b1d09403d0d7b45e081
pavithra-siddaramaiah <pavithrasiddaramaiah@gmail.com>
2024-07-04 16:52:53 -0400

    added setTimeout and loading functionality

--- src/components/BlogList.js
+++ src/components/BlogList.js
@@ -2,14 +2,14 @@ import React from 'react'
 
 const BlogList = (props) => {
     const blogs = props.blogs;
-    const handleDelete = props.handleDelete;
+    
   return (
     <div className = "blog-list">
         {blogs.map((blog) => (
             <div className="blog-preview" key={blog.id}>
                 <h2>{blog.title}</h2>
                 <p>Written by {blog.author}</p>
-                <button onClick = {() => handleDelete(blog.id)}>delete</button>
+                
             </div>
         ))}
     </div>
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,20 +1,28 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import BlogList from './BlogList';
 
 const Home = () => {
-    const [blogs, setBlogs] = useState([
-    { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
-    { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
-    { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
-    ]);
+    const [blogs, setBlogs] = useState(null);
+    const [isPending, setIsPending] = useState(true);
     
-    const handleDelete = (id) => {
-      const newBlogs = blogs.filter((blog) => blog.id !== id)
-        setBlogs(newBlogs)
-    }
+    
+
+    useEffect(() => {
+      setTimeout(() => {
+        fetch('http://localhost:8000/blogs')
+        .then((res) => {
+            return res.json();
+        })
+        .then((data) => {
+          setBlogs(data);
+          setIsPending(false);
+        })
+      }, 1000)
+    }, [])
   return (
     <div className="home">
-        <BlogList blogs = {blogs} handleDelete = {handleDelete} />
+        {isPending && <div>Loading..</div>}
+        {blogs && <BlogList blogs = {blogs} />}
     </div>
   )
 }
