9595ece5db454ccb252c3fd9a49f099e712a3f56
pavithra-siddaramaiah <pavithrasiddaramaiah@gmail.com>
2024-07-04 23:12:29 -0400

    Added Custom hook

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,26 +1,12 @@
 import React, { useEffect, useState } from 'react'
 import BlogList from './BlogList';
+import useFetch from './useFetch';
 
 const Home = () => {
-    const [blogs, setBlogs] = useState(null);
-    const [isPending, setIsPending] = useState(true);
-    
-    
-
-    useEffect(() => {
-      setTimeout(() => {
-        fetch('http://localhost:8000/blogs')
-        .then((res) => {
-            return res.json();
-        })
-        .then((data) => {
-          setBlogs(data);
-          setIsPending(false);
-        })
-      }, 1000)
-    }, [])
+    const {data:blogs, isPending, error} = useFetch('http://localhost:8000/blogs')
   return (
     <div className="home">
+      { error && <div> { error }</div>}
         {isPending && <div>Loading..</div>}
         {blogs && <BlogList blogs = {blogs} />}
     </div>
